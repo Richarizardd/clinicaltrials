374f60d443e14132dc84252ee7ebf4af452d99c7
Daniel Chen <chen.daniel7@gmail.com>
2016-02-06 14:53:36 -0500

    Implemented sponsor rankings

--- mysite/clinicalsearch/scraper.py
+++ mysite/clinicalsearch/scraper.py
@@ -1,18 +1,58 @@
 from bs4 import BeautifulSoup
-from clinical_trials import Trials
 from ClinicalTrial import ClinicalTrial
 import stateQuery
 import csv
 import requests
 
-open_trials = stateQuery.get_state_trials()
-closed_trials = stateQuery.get_closed_trials()
-trials = open_trials + closed_trials
-print "Open trials: ", len(open_trials)
-print "Closed trials: ", len(closed_trials)
+# open_trials = stateQuery.get_state_trials()
+# closed_trials = stateQuery.get_closed_trials()
+# trials = open_trials + closed_trials
+# print "Open trials: ", len(open_trials)
+# print "Closed trials: ", len(closed_trials)
 
-def is_ascii(s):
-    return all(ord(c) < 128 for c in s)
+# Calculate sponsor rankings and write them to a csv file
+def sponsors_impact(sponsors_ctrials):
+	sponsors_impact = {}
+	max_ongoing = 0
+	sponsors_ongoing = {}	# A map from sponser name to the number of ongoing trials
+	for sponsor in sponsors_ctrials:
+		num_completed_results = 0 
+		num_completed = 0
+		num_ongoing = 0
+
+		clinicaltrials = sponsors_ctrials[sponsor]
+		for trial in clinicaltrials:
+			if trial.closed:
+				num_completed += 1
+				if trial.published:
+					num_completed_results += 1
+			else:
+				num_ongoing += 1
+
+		if num_ongoing > max_ongoing:
+			max_ongoing = num_ongoing
+
+		sponsors_ongoing[sponsor] = num_ongoing
+		if num_completed != 0:
+			sponsors_impact[sponsor] = num_completed_results/float(num_completed)
+
+	print sponsors_impact
+	with open('rankings.csv', 'w') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',')
+		for sponsor in sponsors_impact:
+			sponsors_impact[sponsor] += sponsors_ongoing[sponsor] / float(max_ongoing)
+			writer.writerow([sponsor, sponsors_impact[sponsor]])
+
+def test_sponsors_impact():
+	sponsor_to_trials = {}
+	with open('trials.csv', 'r') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',')
+		for row in reader:
+			trial = ClinicalTrial(ID=row[0], sponsor=row[1], published=(row[2] == "True"), state=row[3], url=row[4], closed=(row[5] == "True"), title=row[6], condition=row[7], intervention=row[8], locations=row[9], last_changed=row[10])
+			if trial.sponsor not in sponsor_to_trials:
+				sponsor_to_trials[trial.sponsor] = []
+			sponsor_to_trials[trial.sponsor].append(trial)
+	sponsors_impact(sponsor_to_trials)
 
 # Scrape trials for data and write all trials to a csv file
 def trials_to_csv(trials):
@@ -33,45 +73,18 @@ def trials_to_csv(trials):
 			trial.sponsor = sponsor
 			trial.published = published
 			trial.locations = locations
-			if sponsor not in sponsor_to_trials:
-				sponsor_to_trials[sponsor] = []
-			sponsor_to_trials[sponsor].append(trial)
-			# print "Id: ", is_ascii(trial.id)
-			# print "Sponsor: ", is_ascii(trial.sponsor)
-			# print "State: ", is_ascii(trial.state)
-			# print "URL: ", is_ascii(trial.url)
-			# print "Condition: ", is_ascii(trial.condition)
-			# print "Drug: ", is_ascii(trial.intervention)
 			try: 
 				writer.writerow([trial.id, trial.sponsor, trial.published, trial.state, trial.url, trial.closed, trial.title, trial.condition, trial.intervention.encode('ascii', 'ignore'), trial.locations, trial.last_changed])
+				if sponsor not in sponsor_to_trials:
+					sponsor_to_trials[sponsor] = []
+				sponsor_to_trials[sponsor].append(trial)
 			except UnicodeEncodeError as ude:
 				print "unicode err"
 				continue
 
-def sponsors_impact(sponsors_ctrials_dict):
-	sponsors_impact_dict = {}
-	sponsors = sponsors_ctrials.keys()
 
-	for sponsor in sponsors:
-		num_completed_results = 0 
-		num_completed = 0
-		num_ongoing = 0
-
-		clinicaltrials = sponsors_ctrials_dict[sponsor]
-		for trial in clinicaltrials:
-			if trial.closed:
-				num_completed += 1
-				if trial.published:
-					num_completed_results += 1
-			else:
-				num_ongoing += 1
-
-		sponsors_impact_dict[i] = num_completed_results/float(num_completed)
-
-	return sponsors_impact_dict
-
-trials_to_csv(trials)
-
+#trials_to_csv(trials)
+test_sponsors_impact()
 
 
 		
